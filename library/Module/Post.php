88715b70173aed0bf32f74703291654c51aa98dd
KeNorizon <kenorizon@outlook.com>
2019-01-23 18:26:52 +0800

    modify: post category tree view display

--- library/Module/Post.php
+++ library/Module/Post.php
@@ -2,11 +2,11 @@
 if (!defined('__TYPECHO_ROOT_DIR__')) exit;
 class Icarus_Module_Post
 {
-    private $post;
+    private $_post;
 
     public function __construct($post)
     {
-        $this->post = $post;
+        $this->_post = $post;
     }
 
     public static function config($form)
@@ -21,41 +21,41 @@ class Icarus_Module_Post
 
     public function getPrev()
     {
-        $content = Typecho_Db::get()->fetchRow($this->post->select()->where('table.contents.created < ?', $this->post->created)
+        $content = Typecho_Db::get()->fetchRow($this->_post->select()->where('table.contents.created < ?', $this->_post->created)
             ->where('table.contents.status = ?', 'publish')
-            ->where('table.contents.type = ?', $this->post->type)
+            ->where('table.contents.type = ?', $this->_post->type)
             ->where("table.contents.password IS NULL OR table.contents.password = ''")
             ->order('table.contents.created', Typecho_Db::SORT_DESC)
             ->limit(1));
         if ($content) 
-            return $this->post->filter($content);
+            return $this->_post->filter($content);
         else
             return NULL;
     }
 
     public function getNext()
     {
-        $content = Typecho_Db::get()->fetchRow($this->post->select()->where('table.contents.created > ? AND table.contents.created < ?',
-            $this->post->created, Icarus_Util::$options->time)
+        $content = Typecho_Db::get()->fetchRow($this->_post->select()->where('table.contents.created > ? AND table.contents.created < ?',
+            $this->_post->created, Icarus_Util::$options->time)
             ->where('table.contents.status = ?', 'publish')
-            ->where('table.contents.type = ?', $this->post->type)
+            ->where('table.contents.type = ?', $this->_post->type)
             ->where("table.contents.password IS NULL OR table.contents.password = ''")
             ->order('table.contents.created', Typecho_Db::SORT_ASC)
             ->limit(1));
         if ($content) 
-            return $this->post->filter($content);
+            return $this->_post->filter($content);
         else
             return NULL;
     }
 
     public function hasThumbnail()
     {
-        return Icarus_Util::hasThumbnail($this->post);
+        return Icarus_Util::hasThumbnail($this->_post);
     }
 
     public function getThumbnail()
     {
-        return Icarus_Util::getThumbnail($this->post);
+        return Icarus_Util::getThumbnail($this->_post);
     }
 
     public static function output($post)
@@ -65,15 +65,15 @@ class Icarus_Module_Post
 
     public function doOutput()
     {
-        $isContent = $this->post->is('single');
-        $isPage = $this->post->is('page');
-        $isPost = $this->post->is('post');
+        $isContent = $this->_post->is('single');
+        $isPage = $this->_post->is('page');
+        $isPost = $this->_post->is('post');
 ?>
 <div class="card">
     <?php if ($this->hasThumbnail()): ?>
     <div class="card-image">
-        <?php echo !$isContent ? ('<a href="' . $this->post->permalink . '"') : '<span '; ?> class="image is-7by1">
-            <img class="thumbnail" src="<?php echo $this->getThumbnail(); ?>" alt="<?php $this->post->title(); ?>">
+        <?php echo !$isContent ? ('<a href="' . $this->_post->permalink . '"') : '<span '; ?> class="image is-7by1">
+            <img class="thumbnail" src="<?php echo $this->getThumbnail(); ?>" alt="<?php $this->_post->title(); ?>">
         <?php echo !$isContent ? '</a>' : '</span>' ?>
     </div>
     <?php endif; ?>
@@ -81,15 +81,24 @@ class Icarus_Module_Post
         <?php if (!$isPage): ?>
         <div class="level article-meta is-size-7 is-uppercase is-mobile is-overflow-x-auto">
             <div class="level-left">
-                <time class="level-item has-text-grey" datetime="<?php $this->post->date('c'); ?>"><?php $this->post->date(); ?></time>
-                <?php if ($this->post->categories): ?>
+                <time class="level-item has-text-grey" datetime="<?php $this->_post->date('c'); ?>"><?php $this->_post->date(); ?></time>
+                <?php if ($this->_post->categories): ?>
                 <div class="level-item">
-                <?php $result = array();
-                foreach ($this->post->categories as $category) {
-                    $result[] = '<a class="has-link-grey" href="' . $category['permalink'] . '">'
-                    . $category['name'] . '</a>';
+                <?php 
+                $category = $this->_post->categories[0];
+                $directory = Typecho_Widget::widget('Widget_Metas_Category_List')->getAllParents($category['mid']);
+                $directory[] = $category;
+        
+                if ($directory) {
+                    $result = array();
+        
+                    foreach ($directory as $category) {
+                        $result[] = '<a class="has-link-grey" href="' . $category['permalink'] . '">'
+                        . $category['name'] . '</a>';
+                    }
+        
+                    echo implode('&nbsp;/&nbsp;', $result);
                 }
-                echo implode('&nbsp;/&nbsp;', $result);
                 ?>
                 </div>
                 <?php 
@@ -101,21 +110,21 @@ class Icarus_Module_Post
         <?php endif; ?>
         <h1 class="title is-size-3 is-size-4-mobile has-text-weight-normal">
             <?php if (!$isContent): ?>
-                <a class="has-link-black-ter" href="<?php $this->post->permalink(); ?>"><?php $this->post->title(); ?></a>
+                <a class="has-link-black-ter" href="<?php $this->_post->permalink(); ?>"><?php $this->_post->title(); ?></a>
             <?php else: ?>
-                <?php $this->post->title(); ?>
+                <?php $this->_post->title(); ?>
             <?php endif; ?>
         </h1>
         <div class="content">
-            <?php $this->post->content(); ?>
+            <?php $this->_post->content(); ?>
         </div>
-        <?php if ($isContent && $this->post->tags): ?>
+        <?php if ($isContent && $this->_post->tags): ?>
         <div class="level is-size-7 is-uppercase">
             <div class="level-start">
                 <div class="level-item">
                     <span class="is-size-6 has-text-grey has-mr-7">#</span>
                     <?php $result = array();
-                    foreach ($this->post->tags as $tag) {
+                    foreach ($this->_post->tags as $tag) {
                         $result[] ='<a class="has-link-grey" href="' . $tag['permalink'] . '">'
                         . $tag['name'] . '</a>';
                     }
@@ -129,7 +138,7 @@ class Icarus_Module_Post
         <div class="level is-mobile">
             <div class="level-start">
                 <div class="level-item">
-                <a class="button is-size-7 is-light" href="<?php $this->post->permalink(); ?>#more"><?php _IcTp('article.more'); ?></a>
+                <a class="button is-size-7 is-light" href="<?php $this->_post->permalink(); ?>#more"><?php _IcTp('article.more'); ?></a>
                 </div>
             </div>
         </div>

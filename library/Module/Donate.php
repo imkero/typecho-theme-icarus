e027faded8e0e4064a7940d0a6fbccfe02ab0406
KeNorizon <kenorizon@outlook.com>
2019-02-05 15:13:24 +0800

    modify: donate module

--- library/Module/Donate.php
+++ library/Module/Donate.php
@@ -4,21 +4,5 @@ class Icarus_Module_Donate
 {
     public static function output()
     {
-/*//const services = has_config('donate') ? get_config('donate') : []; ?>
-<?php if (!$services.length > 0) { ?>
-<div class="card">
-    <div class="card-content">
-        <h3 class="menu-label has-text-centered"><?php= __('donate.title') ?></h3>
-        <div class="buttons is-centered">
-            <?php for (let service of services) {
-                const type = get_config_from_obj(service, 'type');
-                if (type !== null) { ?>
-                <?php echo partial('donate/' + type, { type, service }) ?>
-                <?php }
-            } ?>
-        </div>
-    </div>
-</div>
-<?php }*/
     }
 }
